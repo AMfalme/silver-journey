d9e9bb63ea5b6c405e38775cf67cb4172a8329e5
GMfalme <gmfalme@mizizi.io>
2021-02-12 22:49:11 +0300

    change inclusive of oscar templates and make them into the company colors

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e$^4^o)m=vfv-=)(^p_#6ws(a26=72ohbt2i)t4ppwavh-gyet'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['edailyfocus.com', 'localhost']
 
