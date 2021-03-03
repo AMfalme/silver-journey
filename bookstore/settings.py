91fc76d4f195140b615ee889b67d986e082c6e58
GMfalme <gmfalme@mizizi.io>
2021-03-03 12:20:06 +0300

    Change website theme, logo for footer and dashboard. Ensure that media files are saved to the media folder correctly

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -203,5 +203,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# MEDIA_URL = "/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, '/')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
